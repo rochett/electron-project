b486d1d446e90149fa5bcb5f3b1714362989407f
FRANCISCO ROCHETT TAVARES MARTINS <1007996@pmenos.com.br>
2020-01-10 14:38:22 -0300

    Carousel finalizado

--- src/template/components/carrousel/imageSlider.jsx
+++ src/template/components/carrousel/imageSlider.jsx
@@ -1,55 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import AliceCarousel from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css';
 import "react-alice-carousel/lib/alice-carousel.css";
 import Image from 'react-bootstrap/Image';
-import { titulo_secao } from '../../../configapp';
+import firebase from "firebase/app";
 
-class Gallery extends React.Component {
-    state = {
-        galleryItems: [require('../../../image/slider3.png'), 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((i) => <Image src={i} key={i} width="150" height="250" rounded />),
-    }
+export default function Gallery() {
 
-    responsive = {
+    const [carrossel, setCarrossel] = useState([]);
+    const [itemsInSlide, setItemsInSlide] = useState([]);
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+
+        firebase.database().ref('filmes/').on('value', function (_carrossel) {
+            setCarrossel(_carrossel.val());
+        });
+    }, []);
+
+    var filtered = carrossel && carrossel.filter(function (el) {
+        return el != null;
+    });
+
+    const responsive = {
         0: { items: 3 },
-        1024: { items: 8 },
+        1024: { items: 9 },
     }
 
-    onSlideChange(e) {
-        console.debug('Item`s position during a change: ', e.item)
-        console.debug('Slide`s position during a change: ', e.slide)
-    }
-
-    onSlideChanged(e) {
-        console.debug('Item`s position after changes: ', e.item)
-        console.debug('Slide`s position after changes: ', e.slide)
-    }
-
-    handleOnSlideChange = (event) => {
+    const handleOnSlideChange = (event) => {
         const { itemsInSlide, item } = event
-        this.setState({ itemsInSlide, currentIndex: item })
+        setItemsInSlide(itemsInSlide);
+        setItem(item);
     }
 
-    render() {
-        return (
-            <AliceCarousel
-                items={this.state.galleryItems}
-                responsive={this.responsive}
-                autoPlayInterval={2000}
-                autoPlayDirection="rtl"
-                autoPlay={true}
-                fadeOutAnimation={true}
-                mouseTrackingEnabled={true}
-                playButtonEnabled={false}
-                disableAutoPlayOnAction={true}
-                onSlideChange={this.onSlideChange}
-                onSlideChanged={this.onSlideChanged}
-                dotsDisabled={true}
-                onResized={this.handleOnSlideChange}
-                buttonsDisabled={true}
-            />
-        )
-    }
+    return (
+        <AliceCarousel
+            items={filtered && filtered.map((text, i) => <Image src={text.cartaz} key={i} width="170" height="250" rounded title={text.titulo} />)}
+            responsive={responsive}
+            autoPlayInterval={4000}
+            autoPlayDirection="rtl"
+            autoPlay={true}
+            fadeOutAnimation={true}
+            mouseTrackingEnabled={true}
+            playButtonEnabled={false}
+            disableAutoPlayOnAction={false}
+            dotsDisabled={true}
+            onResized={handleOnSlideChange}
+            buttonsDisabled={true}
+        />
+    );
 }
-
-export default Gallery
